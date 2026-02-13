c7f599519c41362d9fa7c255a245096e84bd07a8
Southpaw <git@southpaw1496.dev>
2026-02-13 18:13:51 +0000

    Apply Starlight’s suggested CSS theme with our colours
    https://starlight.astro.build/guides/css-and-tailwind/#custom-css-styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
+			customCss: [
+				"./src/styles/theme.css"
+			],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
