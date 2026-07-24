eafaba14bbea3ae0a370508aee7ef2cc8f030cf3
Southpaw <git@southpaw1496.dev>
2026-07-24 17:36:19 +0100

    Add under construction banner

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,14 @@
 import { defineCollection } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { z } from 'astro/zod';
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	docs: defineCollection({ loader: docsLoader(), schema: docsSchema({
+		extend: z.object({
+			banner: z.object({ content: z.string() }).default({
+				content: "🚧 This documentation is still under construction and very experimental 🚧"
+			})
+		})
+	}) }),
 };
